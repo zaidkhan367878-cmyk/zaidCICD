6a38631a126b2f839a86a874872180db31d5bf84
Zaid Khan <zaidkhan367878@gmail.com>
2026-05-02 15:13:51 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/zmr/helloworld/Restcontroller/HelloWorldRestController.java
@@ -0,0 +1,12 @@
+package com.zmr.helloworld.Restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldRestController {
+	
+	
+
+}
